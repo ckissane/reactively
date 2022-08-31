57580b4d8cb57907ae54d392f977ed4c7b7e0700
Cole Kissane <cole.kissane@gmail.com>
2022-08-30 21:25:18 -0700

    gg

--- benches/core.js
+++ benches/core.js
@@ -4,46 +4,65 @@ let effectQueue = [];
 
 export class Reactive {
   constructor(fnOrValue, effect) {
-    this.state = 2 /* CacheState.DIRTY */;
-    if (typeof fnOrValue === "function") {
-      this.fn = fnOrValue;
-      this.value = undefined;
-      this.effect = effect;
-      if (effect) this.update();
-    } else {
-      this.fn = undefined;
-      this.value = fnOrValue;
-      this.effect = false;
-    }
+    this.qq = 0;
     this.sources = null;
     this.sourceSlots = null;
     this.observers = null;
     this.observerSlots = null;
+    if (typeof fnOrValue === "function") {
+      this.state = 1
+      this.fn = fnOrValue;
+      this.value = undefined;
+      this.effect = effect;
+    } else {
+      this.fn = undefined;
+      this.value = fnOrValue;
+      this.effect = false;
+      this.state = 0;
+    }
+    if (effect) this.update();
   }
-  stale(state) {
-    if (this.state < state) {
-      this.state = state;
-      if (this.effect) effectQueue.push(this);
-      else if (this.observers) {
+  stale(sway) {
+    const a = this.state > 0;
+    this.state += sway;
+    const b = this.state > 0;
+    if (a ^ b) {
+      if (b) {
+        if (this.effect && this.qq === 0) {
+          effectQueue.push(this);
+          this.qq++;
+        }
+      }
+      const c=-1+(b<<1);
+      if (!this.effect && this.observers) {
         for (let i = 0; i < this.observers.length; i++) {
-          this.observers[i].stale(1 /* CacheState.CHECK */);
+          this.observers[i].stale(c);
         }
       }
     }
   }
+  
   set(value) {
-    if (this.value !== value && this.observers) {
-      for (let i = 0; i < this.observers.length; i++) {
-        this.observers[i].stale(2 /* CacheState.DIRTY */);
-      }
-    }
+    const oldValue = this.value;
     this.value = value;
+    // this.stale(-this.state);
+    // if ((oldValue !== this.value) && this.observers) {
+    //   for (let i = 0; i < this.observers.length; i++) {
+    //     this.observers[i].stale(1);
+    //   }
+    // }
+    if ((oldValue !== this.value)){
+      if(this.state===0){
+        this.stale(1);
+      }
+      // this.stale(-this.state,(oldValue !== this.value));
+    }else{
+      this.stale(-this.state);
+    }
+    this.state=0;
   }
 
   update() {
-    if (this.sources) {
-      cleanNode(this);
-    }
     const oldValue = this.value;
     const listener = CurrentCompute;
     CurrentCompute = this;
@@ -52,31 +71,39 @@ export class Reactive {
     } finally {
       CurrentCompute = listener;
     }
-
-    this.state = 0 /* CacheState.CURRENT */;
-    if (oldValue !== this.value && this.observers) {
-      for (let i = 0; i < this.observers.length; i++) {
-        this.observers[i].stale(2 /* CacheState.DIRTY */);
+    if ((oldValue !== this.value)){
+      if(this.state===0){
+        this.stale(1);
       }
+      // this.stale(-this.state,(oldValue !== this.value));
+    }else{
+      this.stale(-this.state);
     }
+    this.state=0;
+    // if ((oldValue !== this.value) && this.observers) {
+    //   for (let i = 0; i < this.observers.length; i++) {
+    //     this.observers[i].stale(1);
+    //   }
+    // }
   }
 
   updateIfNecessary() {
-    if (this.state == 2 /* CacheState.DIRTY */) {
-      return this.update();
-    }
-    if (this.state == 1 /* CacheState.CHECK */) {
+    if (!this.fn) return;
+    if (this.state > 0) {
       if (this.sources) {
         for (let i = 0; i < this.sources.length; i++) {
           this.sources[i].updateIfNecessary();
         }
-        if (this.state == 2 /* CacheState.DIRTY */) {
-          return this.update();
-        }
       }
-      this.state = 0 /* CacheState.CURRENT */;
+      if (this.state > 0) {
+        if (this.sources) {
+          cleanNode(this);
+        }
+        this.update();
+      }
     }
   }
+
   get() {
     if (CurrentCompute) {
       const sSlot = this.observers ? this.observers.length : 0;
@@ -87,6 +114,13 @@ export class Reactive {
         CurrentCompute.sources.push(this);
         CurrentCompute.sourceSlots.push(sSlot);
       }
+    }
+    const ss = this.state;
+    this.updateIfNecessary();
+    if (CurrentCompute) {
+      if (ss * this.state > 0) {
+        CurrentCompute.stale(1);
+      }
       if (!this.observers) {
         this.observers = [CurrentCompute];
         this.observerSlots = [CurrentCompute.sources.length - 1];
@@ -95,7 +129,7 @@ export class Reactive {
         this.observerSlots.push(CurrentCompute.sources.length - 1);
       }
     }
-    if (this.fn) this.updateIfNecessary();
+
     return this.value;
   }
 }
@@ -133,6 +167,7 @@ export function computed(fn) {
 
 export function stabilize() {
   for (let i = 0; i < effectQueue.length; i++) {
+    effectQueue[i].qq--;
     effectQueue[i].get();
   }
   effectQueue.length = 0;
